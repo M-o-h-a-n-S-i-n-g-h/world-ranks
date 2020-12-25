38482fc43a28127d224862a898da27abcef424f0
Mohansingh Omprakash <mohan.singh1385664@gmail.com>
2020-12-25 08:32:52 +0530

    In the stage of Not working commited to see the diff

--- src/Components/CountriesTable/CountriesTable.component.jsx
+++ src/Components/CountriesTable/CountriesTable.component.jsx
@@ -1,17 +1,13 @@
-import {
-  KeyboardArrowDown,
-  KeyboardArrowDownRounded,
-  KeyboardArrowUp,
-} from "@material-ui/icons";
+import { KeyboardArrowDown, KeyboardArrowUp } from "@material-ui/icons";
 import { useState } from "react";
 import styles from "./CountriesTable.module.css";
 
-const sortCountries = (countries, direction) => {
+const sortCountries = (countries, value, direction) => {
   if (direction === "desc") {
-    return countries.sort((a, b) => (a.population < b.population ? 1 : -1));
+    return countries.sort((a, b) => (a[value] < b[value] ? 1 : -1));
   }
   if (direction === "asc") {
-    return countries.sort((a, b) => (a.population > b.population ? 1 : -1));
+    return countries.sort((a, b) => (a[value] > b[value] ? 1 : -1));
   }
 };
 
@@ -29,7 +25,7 @@ const CountriesTable = ({ countries }) => {
   const [direction, setDirection] = useState();
   const [value, setValue] = useState();
 
-  const orderedCountries = sortCountries(countries, "desc");
+  const orderedCountries = sortCountries(countries, value, direction);
 
   const switchDirection = () => {
     if (!direction) {
@@ -41,16 +37,27 @@ const CountriesTable = ({ countries }) => {
     }
   };
 
+  const switchDirectionAndValue = (value) => {
+    switchDirection();
+    setValue(value);
+  };
+
   return (
     <div className={styles.country__container}>
       <div className={styles.heading}>
-        <button className={styles.heading__name} onClick={switchDirection}>
+        <button
+          className={styles.heading__name}
+          onClick={() => switchDirectionAndValue("name")}
+        >
           <div>Name</div>
           <SortTable direction={direction} />
         </button>
-        <button className={styles.heading__population}>
+        <button
+          className={styles.heading__population}
+          onClick={() => switchDirectionAndValue("population")}
+        >
           <div>Population </div>
-          {/* <SortTable direction={direction} /> */}
+          <SortTable direction={direction} />
         </button>
       </div>
       {orderedCountries.map((country, index) => {
